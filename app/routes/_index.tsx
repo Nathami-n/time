24bb1abf41c16c3387988fe027b9e95e9098930b
Nathami-n <georgenathan010@gmail.com>
2024-12-18 09:15:13 +0300

    add: the demarcation of users

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,10 @@
-import { Link } from "@remix-run/react";
+import { LoaderFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
 import { LightbulbIcon, TimerIcon, TimerOffIcon } from "lucide-react";
+import { PossibleUsers } from "types/types";
 import { Logo } from "~/components/logo";
-import { Button } from "~/components/ui/button";
+import { Button, buttonVariants } from "~/components/ui/button";
+import { userCookie } from "~/lib/user-session";
 
 
 const links = [
@@ -22,11 +25,20 @@ const links = [
   }
 ]
 
+export const loader: LoaderFunction = async ({ request }: LoaderFunctionArgs) => {
+  const serializedUser = request.headers.get("cookie");
+  const user = await userCookie.parse(serializedUser);
+  return Response.json({ user }, { status: 200 });
+
+}
+
 export default function HomePage() {
-  
+
+  const { user } = useLoaderData<typeof loader>();
+
   return (
     <div className="min-h-screen w-full relative">
-       <div
+      <div
         className="absolute  -z-20 w-[1000px] h-[1000px] rounded-full -top-32 -left-32 opacity-10"
         style={{
           background: "conic-gradient(from 0deg, #4facfe 0%, #00f2fe 50%, #4facfe 100%)",
@@ -53,7 +65,13 @@ export default function HomePage() {
           ))}
         </ul>
         <div className="">
-          <Button variant={"secondary"} className="shadow-md text-primary">Start here</Button>
+          <Link
+            to={user.role === PossibleUsers.ADMIN ? "/dashboard" : user.role === PossibleUsers.STUDENT ? "/student" : "/teacher"}
+            className={buttonVariants({
+              className: "!shadow-md !text-primary",
+              variant: "secondary"
+            }
+            )}>{user ? "Dashboard" : "Start here"}</Link>
         </div>
       </nav>
 
@@ -61,11 +79,11 @@ export default function HomePage() {
       <div className="mt-[80px] max-md:px-2 max-w-xl mx-auto flex flex-col items-center space-y-1">
         <p className="uppercase text-sm text-primary max-md:text-xs"> An automated Timetable</p>
         <p className=" max-md:text-3xl text-5xl font-semibold flex items-center">Redeem your time <TimerIcon className="stroke-primary" size={40} />,</p>
-        <p className=" max-md:text-3xl text-5xl gap-x-1 font-semibold flex items-center "> Move <LightbulbIcon size={40} className="bg-yellow-400 stroke-primary rounded-lg p-1"/> with speed</p>
+        <p className=" max-md:text-3xl text-5xl gap-x-1 font-semibold flex items-center "> Move <LightbulbIcon size={40} className="bg-yellow-400 stroke-primary rounded-lg p-1" /> with speed</p>
         <p className="text-muted-foreground max-md:text-sm">Generate and onboard tutors and students in no time</p>
-          <div className="mt-6 ring-4 outline-4 rounded-lg shadow-xl ring-inset  border p-4">
-            <img src="/table.png" alt="home" />
-          </div>
+        <div className="mt-6 ring-4 outline-4 rounded-lg shadow-xl ring-inset  border p-4">
+          <img src="/table.png" alt="home" />
+        </div>
       </div>
 
     </div>
